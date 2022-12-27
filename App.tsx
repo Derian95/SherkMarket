3a6d0d2e32ad2f3ae932ef35c6820c964f074918
Derian95 <dfhamezquita@gmail.com>
2022-12-27 11:38:29 -0500

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View,TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Shrek Market</Text>
+      <TextInput
+        value={"usuario"}
+      />
+        <TextInput
+        value={"contraseña"}
+      />
+
+      <Button
+      title='INGRESAR'
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/auth/autentification.ts
@@ -0,0 +1,12 @@
+
+
+const logIn=(user:string, password:string):boolean => {
+  
+    let validate:boolean;
+    validate=user=='rafaela' && password=='refaela123'
+
+    return validate
+
+}
+
+
